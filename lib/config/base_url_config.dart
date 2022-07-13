5c026d26e0c299ce29fef39a8a9c5102860173bc
Arnan Theopilus <arnantheofilus@gmail.com>
2022-07-13 15:53:46 +0700

    add: initial base movie zone

---
+++ lib/config/base_url_config.dart
@@ -0,0 +1,4 @@
+class BaseUrlConfig {
+  final String baseUrlDevelopment = 'https://api.themoviedb.org/3/';
+  // final String baseUrlProduction = 'https://newsapi.org';
+}
---
+++ lib/config/constant_config.dart
@@ -0,0 +1,4 @@
+class ConstantConfig {
+  // API Key (v3 auth)
+  static const String keyMovieApi = 'd0bdd48ffbc0df8fdbac5fb0b05f3623';
+}
